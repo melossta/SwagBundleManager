62cfc89e39e15a947bdcc2099346412305bf865d
Melos <statovcimelos96@gmail.com>
2025-08-20 11:40:21 +0200

    Add products showing on bundle tab

--- src/Storefront/Page/Product/Subscriber/ProductPageCriteriaSubscriber.php
+++ src/Storefront/Page/Product/Subscriber/ProductPageCriteriaSubscriber.php
@@ -20,7 +20,7 @@ class ProductPageCriteriaSubscriber implements EventSubscriberInterface
 
     public function onProductPageCriteria(ProductPageCriteriaEvent $event): void
     {
-        $event->getCriteria()->addAssociation('bundles.product.cover');
+        $event->getCriteria()->addAssociation('bundles.products.cover');
     }
 
 }
